bb424ae462fb12dc0c210708255d45ac2380ea68
FrederickGSDev <fredgs.dev@gmail.com>
2024-11-12 17:20:14 -0600

    refactor: remove console logs from copyUrl function

--- src/utils/copyUrl.js
+++ src/utils/copyUrl.js
@@ -4,9 +4,6 @@ export function copyUrl (buttonElement) {
   const domain = window.location.origin
   const base64 = base64Store.get().base64 === '' ? '' : `${base64Store.get().base64}`
   const url = `${domain}${base64}`
-  console.log(domain)
-  console.log(base64)
-  console.log(url)
   navigator.clipboard.writeText(url)
 
   buttonElement.innerHTML = '<svg  xmlns="http://www.w3.org/2000/svg"  width="30"  height="30"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icon-tabler-check icons-tabler-outline"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l5 5l10 -10" /></svg>'
