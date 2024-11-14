778e2a15308a996122a2ae3cbdd206e55893e98b
FrederickGSDev <fredgs.dev@gmail.com>
2024-11-13 20:59:49 -0600

    fix: initialize designStore with saved design from localStorage

--- src/stores/designStore.js
+++ src/stores/designStore.js
@@ -4,7 +4,7 @@ import { setDesign } from '../utils/setFromLocal/setDesign'
 
 const SAVED_DESIGN = window.localStorage.getItem('design') || 'grid'
 
-export const designStore = atom('grid')
+export const designStore = atom(SAVED_DESIGN)
 
 designStore.listen((value) => {
   setDesign(value)
